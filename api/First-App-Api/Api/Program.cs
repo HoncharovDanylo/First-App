244c078943dc594efb859b6ffb229a5360183781
Danylo <danylohoncharov15@gmail.com>
2024-04-22 17:34:51 +0300

    Creating DbContext, and set up connection string

--- api/First-App-Api/Api/Program.cs
+++ api/First-App-Api/Api/Program.cs
@@ -1,8 +1,13 @@
+using Api.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
