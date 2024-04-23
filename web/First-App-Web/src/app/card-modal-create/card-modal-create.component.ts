b229c4a2fbb877d4f58ef26e97ddea379025b13c
Danylo <danylohoncharov15@gmail.com>
2024-04-23 12:00:41 +0300

    web-api dev before megin the branches

---
+++ web/First-App-Web/src/app/card-modal-create/card-modal-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-modal-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-modal-create.component.html',
+  styleUrl: './card-modal-create.component.css'
+})
+export class CardModalCreateComponent {
+
+}
---
+++ web/First-App-Web/src/app/models/card.model.ts
@@ -0,0 +1,9 @@
+export interface Card {
+  id : number,
+  title : string,
+  description : string,
+  priority : string,
+  dueDate : Date,
+  taskListName : string,
+  taskListId : number
+}
