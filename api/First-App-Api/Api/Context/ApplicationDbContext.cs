c75a897676c9a12039724122f7066f8e1107d6f3
Danylo <danylohoncharov15@gmail.com>
2024-04-29 10:06:23 +0300

    Added data seeding in ApplicationDbContext

--- api/First-App-Api/Api/Context/ApplicationDbContext.cs
+++ api/First-App-Api/Api/Context/ApplicationDbContext.cs
@@ -25,7 +25,37 @@ public class ApplicationDbContext : DbContext
         modelBuilder.Entity<TaskList>().HasIndex(t => t.Name).IsUnique();
         modelBuilder.Entity<Card>().HasOne(x => x.TaskList)
             .WithMany(x => x.Cards).HasForeignKey(x => x.TaskListId).OnDelete(DeleteBehavior.Cascade);
-
+        
+        // Data seeding
+        var lists = new List<TaskList>()
+        {
+            new TaskList() { Id =1, Name = "To Do" },
+            new TaskList() { Id =2, Name = "In progress" },
+        };
+        var cards = new List<Card>()
+        {
+            new Card() { Id=1, Title = "Create structure", Description = "This is the first card", Priority = "Low", DueDate = DateOnly.Parse("2024-05-01"),TaskListId = 2 },
+            new Card() { Id=2, Title = "Setup Entity Framework", Description = "This is the second card", Priority = "High", DueDate = DateOnly.Parse("2024-05-10"),TaskListId = 2 },
+            new Card() { Id=3, Title = "Create models", Description = "This is the third card", Priority = "Medium", DueDate = DateOnly.Parse("2024-05-24"),TaskListId = 2 },
+            new Card() { Id=4, Title = "Develop controller for lists", Description = "This is the fourth card", Priority = "Medium", DueDate = DateOnly.Parse("2024-06-05"),TaskListId = 2 },
+            new Card() { Id=5, Title = "Implement validation", Description = "This is the fifth card", Priority = "Low", DueDate = DateOnly.Parse("2024-06-12"),TaskListId = 2 },
+            new Card() { Id=6, Title = "Create front-end", Description = "This is the sixth card", Priority = "Low", DueDate = DateOnly.Parse("2024-06-14"),TaskListId = 1 },
+            new Card() { Id=7, Title = "Test application", Description = "This is the seventh card", Priority = "High", DueDate = DateOnly.Parse("2024-06-19"),TaskListId = 1 },
+            new Card() { Id=8, Title = "BugFix", Description = "This is the eight card", Priority = "High", DueDate = DateOnly.Parse("2024-06-19"),TaskListId = 1 },
+            new Card() { Id=9, Title = "Containerize application", Description = "This is the ninths card", Priority = "Medium", DueDate = DateOnly.Parse("2024-06-23"),TaskListId = 1 },
+            new Card() { Id=10, Title = "Push into master branch", Description = "This is the tenth card", Priority = "Low", DueDate = DateOnly.Parse("2024-06-27"),TaskListId = 1 },
+        };
+        var histories = new List<History>();
+        var historyId = 1;
+        foreach (var card in cards)
+        {
+            var hist = _historyService.TrackCreation(card);
+            hist.Id = historyId++;
+            histories.Add(hist);
+        }
+        modelBuilder.Entity<TaskList>().HasData(lists);
+        modelBuilder.Entity<Card>().HasData(cards);
+        modelBuilder.Entity<History>().HasData(histories);
         base.OnModelCreating(modelBuilder);
     }
 
